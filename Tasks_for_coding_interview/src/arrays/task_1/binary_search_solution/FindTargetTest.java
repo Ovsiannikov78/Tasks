fc2478fa1fa0315938b4343d2a6fb415ae27fc10
Sergi <sergi_tanya@mail.ru>
2021-08-20 07:56:45 +0200

    Tasks with arrays - task 1 in 2 variations (binary search and two pointer)

---
+++ Tasks_for_coding_interview/src/arrays/task_1/binary_search_solution/FindTargetTest.java
@@ -0,0 +1,74 @@
+package arrays.task_1.binary_search_solution;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindTargetTest {
+
+    private FindTarget findTarget = new FindTarget();
+    int[] numbers = {1,2,4,7,9,15,20,30,45,55};
+
+    @Test
+    void testFindTargetNum_9() {
+        int targetNum = 9;
+        boolean akt = findTarget.findTargetNum(numbers, targetNum);
+
+        assertTrue(akt);
+    }
+    @Test
+    void testFindTargetNum_10() {
+        int targetNum = 10;
+        boolean akt = findTarget.findTargetNum(numbers, targetNum);
+
+        assertTrue(akt);
+    }
+    @Test
+    void testFindTargetNum_6() {
+        int targetNum = 6;
+        boolean akt = findTarget.findTargetNum(numbers, targetNum);
+
+        assertTrue(akt);
+    }
+
+    @Test
+    void testFindTargetNum_5() {
+        int targetNum = 5;
+        boolean akt = findTarget.findTargetNum(numbers, targetNum);
+
+        assertTrue(akt);
+    }
+
+    @Test
+    void testFindTargetNum_29() {
+
+        int targetNum = 29;
+        boolean akt = findTarget.findTargetNum(numbers, targetNum);
+
+        assertTrue(akt);
+    }
+
+    @Test
+    void testFindTargetNum_100() {
+        int targetNum = 100;
+        boolean akt = findTarget.findTargetNum(numbers, targetNum);
+
+        assertTrue(akt);
+    }
+
+    @Test
+    void testFindTargetNum_7() {
+        int targetNum = 7;
+        boolean akt = findTarget.findTargetNum(numbers, targetNum);
+
+        assertFalse(akt);
+    }
+
+    @Test
+    void testFindTargetNum_20() {
+        int targetNum = 20;
+        boolean akt = findTarget.findTargetNum(numbers, targetNum);
+
+        assertFalse(akt);
+    }
+}
---
+++ Tasks_for_coding_interview/src/arrays/task_1/two_pointer_solution/FindTarget.java
@@ -0,0 +1,30 @@
+package arrays.task_1.two_pointer_solution;
+
+/**
+ * Given a sorted array of integers
+ * and a target sum T
+ * Return true if there exists a pair in the array, that sums to T
+ */
+
+// Performance <<<==== O(n) ====>>>
+
+public class FindTarget {
+
+    public boolean findTargetNum(int[] numbers, int T) {
+
+        int start = 0;
+        int end = numbers.length - 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int searchedNumber = numbers[start] + numbers[end];
+            if (searchedNumber == T) {
+                return true;
+            } else if (searchedNumber > T) {
+                end -= 1;
+            } else {
+                start += 1;
+            }
+        }
+        return false;
+    }
+}
